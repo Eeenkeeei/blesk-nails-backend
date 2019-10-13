f761e2b1cef757f808c4394b256dacb0601ccb0a
romanilicev <shiftr123>
2019-10-13 16:24:56 +0300

    Метод для создания полей и обновления записей

--- src/rest.js
+++ src/rest.js
@@ -10,6 +10,7 @@ const bcrypt = require('bcryptjs');
 const moment = require('moment');
 const dateFormatForMoment = 'Do MMMM YYYY, HH:mm:ss';
 const uuidv4 = require('uuid/v4');
+moment.locale('ru');
 
 server.use(restify.plugins.bodyParser());
 server.use(restify.plugins.queryParser());
@@ -26,15 +27,70 @@ const mongoClient = new MongoClient(url, {
     useUnifiedTopology: true
 });
 
-let collection;
-let news;
-let log;
+let records;
+
+const returnDaysInMonth = (year) => {
+    let startDay = 1;
+    const days = {};
+    while (startDay <= moment(year, "YYYY-MM").daysInMonth()) {
+        days[startDay] = {
+            1: {
+                id: uuidv4(),
+                time: '',
+                comment: '',
+                cost: 0
+            },
+            2: {
+                id: uuidv4(),
+                time: '',
+                comment: '',
+                cost: 0
+            },
+            3: {
+                id: uuidv4(),
+                time: '',
+                comment: '',
+                cost: 0
+            },
+            4: {
+                id: uuidv4(),
+                time: '',
+                comment: '',
+                cost: 0
+            },
+        };
+        startDay++
+    }
+    return days
+};
 
 mongoClient.connect(function (err, client) {
-        const db = client.db("heroku_gj1rg06b");
-        collection = db.collection("records");
-    }
-);
+    const db = client.db("heroku_gj1rg06b");
+    records = db.collection("records");
+    // records.insertOne({
+    //     "year": "2019",
+    //     "2019": {
+    //         "10": returnDaysInMonth("2019-10"),
+    //         "11": returnDaysInMonth("2019-11"),
+    //         "12": returnDaysInMonth("2019-12")
+    //     }
+    // })
+    records.find({year: "2019"}).toArray(function (err, result) {
+        let response = result[0];
+        records.updateOne({year: "2019"}, {$set: { "2019": returnUpdatedDay(result[0], 2019, 10, 1, 3, '9:00', 'comment', 500)["2019"]}})
+    });
+});
+
+const returnUpdatedDay = (result, year, month, day, recordNumber, time, comment, cost) => {
+    let response = result;
+    response[year][month][day][recordNumber] = {
+        id: response[year][month][day][recordNumber]["id"],
+        time,
+        comment,
+        cost
+    };
+    return response
+};
 
 
 server.pre((req, res, next) => {
@@ -50,215 +106,10 @@ server.pre((req, res, next) => {
 });
 
 
-server.get('/getSupportList', (req, res, next) => {
-    collection.find().toArray(function (err, results) {
-        res.send(results);
-        next();
-    });
-});
-
-server.get('/getNewsList', (req, res, next) => {
-    news.find().toArray(function (err, results) {
-        res.send(results);
-        next();
-    });
-});
-
-server.post('/removeNews', (req, res, next) => {
-    news.deleteOne({body: req.body.body}, function (err, result) {
-    });
-    res.send(true)
-});
-
-server.post('/editNews', (req, res, next) => {
-    console.log(req.body);
-    news.replaceOne({body: req.body.oldBody, header: req.body.oldHeader}, {
-        body: req.body.body,
-        header: req.body.header,
-        author: req.body.author,
-        img: req.body.img,
-        data: moment().format(dateFormatForMoment)
-    }, () => {
-        res.send(true)
-    });
-});
-
-
-server.post('/addAdmin', (req, res, next) => {
-    collection.find({email: req.body.email}).toArray((err, result) => {
-        if (result[0] !== undefined) {
-            collection.updateOne({email: req.body.email}, {$set: {admin: true}});
-            res.send(true)
-        }
-    });
-    collection.find({email: req.body.email}).toArray((err, result) => {
-        if (result.length === 0) {
-            res.send('false')
-        }
-    });
-    next()
-});
-
-server.post('/getUserDataFromAdmin', (req, res, next) => {
-    collection.find({email: req.body.email}).toArray((err, result) => {
-        if (result[0] !== undefined) {
-            res.send(result[0])
-        }
-    });
-    collection.find({email: req.body.email}).toArray((err, result) => {
-        if (result.length === 0) {
-            res.send('false')
-        }
-    });
-    next()
-});
-
-server.post('/removeUserAdmin', (req, res, next) => {
-    collection.find({email: req.body.email}).toArray((err, result) => {
-        if (result[0] !== undefined) {
-            collection.updateOne({email: req.body.email}, {$set: {admin: false}});
-            res.send(true)
-        }
-    });
-    collection.find({email: req.body.email}).toArray((err, result) => {
-        if (result.length === 0) {
-            res.send('false')
-        }
-    });
-    next()
-});
-
-moment.locale('ru');
-console.log(moment().utcOffset())
-
-server.post('/deleteAccount', (req, res, next) => {
-    collection.remove({email: req.body.email});
-    res.send(true);
-    next()
-});
-
-server.post('/addNews', (req, res, next) => {
-    let object = {
-        header: req.body.header,
-        author: req.body.author,
-        body: req.body.body,
-        img: req.body.img,
-        data: moment().format(dateFormatForMoment)
-    };
-
-    news.insertOne(object, function (err, result) {
-        res.send(true);
-        if (err) {
-            return console.log(err);
-        }
-    });
-});
-
-server.get('/user', (req, res, next) => {
-    console.log('GET USER');
-    console.log(req.user.username, req.user.password);
-    const decodedObject = jwt.verify(req.headers.authorization.split(' ')[1], config.jwt.secret);
-    console.log(decodedObject);
-    user.returnUpdatedObject(decodedObject.username, decodedObject.password).then((data, e) => {
-        try {
-            if (data === null) {
-                res.send('Null');
-            }
-            res.send(data);
-            collection.updateOne({email: data.email}, {$set: {lastLoginDate: moment().format(dateFormatForMoment)}});
-            next()
-        } catch (e) {
-            return next(new InvalidCredentialsError());
-        }
-    });
-});
-
-server.post('/auth', (req, res, next) => {
-    let {email, password} = req.body;
-    console.log(email, password);
-    user.authenticate(email, password).then((data, e) => {
-        try {
-            if (data === null) {
-                return next(new InvalidCredentialsError());
-            }
-            const dataForToken = {
-                password: data.password,
-                email: data.email
-            }
-            let token = jwt.sign(dataForToken, config.jwt.secret, {
-                expiresIn: '7d'
-            });
-
-            let {iat, exp} = jwt.decode(token);
-            res.send({iat, exp, token});
-            next()
-        } catch (e) {
-            return next(new InvalidCredentialsError());
-
-        }
-    });
-});
-
-let resultFlag = '';
-
-server.post('/changePassword', (req, res, next) => {
-    console.log('CHANGE PASSWORD');
-    let userData = req.body;
-    let oldPassword = req.body.oldPassword;
-    let newPassword = req.body.newPassword;
-    let confirmNewPassword = req.body.confirmNewPassword;
-    if (newPassword.length < 7) {
-        console.log('Длина пароля меньше 8');
-        res.send('Bad password length');
-        next();
-        return;
-    }
-    if (newPassword !== confirmNewPassword) {
-        console.log('Пароли не совпадают');
-        res.send('Bad confirm');
-        next();
-        return;
-    }
-    mongoClient.connect(async function (err, client) {
-        const db = client.db("heroku_gj1rg06b");
-        const collection = db.collection("users");
-        collection.find({username: req.body.username}).toArray(function (err, result) {
-            if (result.length !== 0) {
-                userData = result[0];
-                if (bcrypt.compareSync(newPassword, userData.password) === true) {
-                    console.log('Старый и новый пароль совпадает');
-                    res.send('Passwords matches');
-                    next();
-                    return;
-                }
-                if (bcrypt.compareSync(oldPassword, userData.password) === true) {
-                    console.log('Пароль обновлен');
-                    let newData = {
-                        username: userData.username,
-                        password: confirmNewPassword
-                    };
-                    collection.updateOne({username: userData.username}, {$set: {password: bcrypt.hashSync(confirmNewPassword, 10)}});
-                    // console.log(newData);
-                    res.send('Updated');
-                    return;
-                }
-                if (bcrypt.compareSync(oldPassword, userData.password) === false) {
-                    console.log('Старый пароль не совпадает');
-                    res.send('Not confirmed');
-                    return;
-                }
-            }
-        });
-    });
-    next();
-});
-
 server.post('/updateData', (req, res, next) => {
     console.log('UPDATE DATA');
     let userData = req.body;
     console.log(userData);
-
-
     collection.replaceOne({email: req.body.email}, {
         email: req.body.email,
         password: req.body.password,
@@ -275,68 +126,11 @@ server.post('/updateData', (req, res, next) => {
     next();
 });
 
-server.post('/registration', (req, res, next) => {
-    console.log('РЕГИСТРАЦИЯ');
-    let hashedPassword = bcrypt.hashSync(req.body.password, 10);
-
-    if (req.body.password.length < 8) {
-        res.send('8 symbols');
-        next();
-        return;
-    }
-
-    if (req.body.password !== req.body.confirmPassword) {
-        console.log('Пароли не совпадают');
-        res.send('Passwords Not Confirmed');
-        next();
-        return;
-    }
-
-
-    // TODO: ввести проверку валидности мыла
-
-    const userForDataBase = newUser.newUserConstructor(hashedPassword, req.body.email);
-
-    mongoClient.connect(function (err, client) {
-        const db = client.db("heroku_gj1rg06b");
-        const collection = db.collection("users");
-        collection.find({email: req.body.email}).toArray(function (err, result) {
-            if (result.length === 0) {
-                resultFlag = 'true';
-                console.log('Копий нет');
-                collection.insertOne(userForDataBase, function (err, result) {
-                    console.log('Добавлено');
-
-                    let token = jwt.sign(userForDataBase, config.jwt.secret, {
-                        expiresIn: '1d'
-                    });
-
-                    let {iat, exp} = jwt.decode(token);
-                    console.log('token', token);
-                    res.send({iat, exp, token});
-                    next()
-                    if (err) {
-                        return console.log(err);
-                    }
-                });
-            } else {
-                resultFlag = 'false';
-                console.log(resultFlag);
-                console.log('Есть копия, не добавлено');
-                res.send('This email already registered');
-                if (err) {
-                    return console.log(err);
-                }
-            }
-        });
-    });
-    next();
-});
 
 const port = process.env.PORT || 7777;
 
 server.listen(port, () => {
-    console.log('Сервер запущен');
+    console.log('Сервер запущен', moment().format(dateFormatForMoment));
 });
 
 
